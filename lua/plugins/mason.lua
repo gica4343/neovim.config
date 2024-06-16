7608b9946b7dd174da958814c614d5fc3b8c7b11
gica <gica4343@tutanota.com>
2024-06-16 13:42:27 +0300

    m

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,8 @@ return {
     "williamboman/mason-lspconfig.nvim",
     config = function()
       require("mason-lspconfig").setup({
-        ensure_installed = { "lua_ls", "clangd", "rust_analyzer", "pylsp" }
+        ensure_installed = { "lua_ls", "clangd", "rust_analyzer", "pylsp" },
+        automatic_installation = true
       })
     end
   },
